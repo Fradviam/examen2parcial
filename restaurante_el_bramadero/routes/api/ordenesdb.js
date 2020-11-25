b2c9fa445199e18cac178e1c93ddb2c4327eae6f
Fradviam <fradviam@gmail.com>
2020-11-24 18:57:24 -0600

    update

--- restaurante_el_bramadero/routes/api/ordenesdb.js
+++ restaurante_el_bramadero/routes/api/ordenesdb.js
@@ -59,4 +59,15 @@ router.get("/one/:id", async (req, res, next) => {
       res.status(500).json({ mensaje: "algo salio mal" });
     }
   });
+  router.put("/update/:id", async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const { state } = req.body;
+      const result = await modelDataOrden.updateOneById(id, state);
+      res.status(200).json(result);
+    } catch (error) {
+      console.log(error);
+    }
+  });
+  
 module.exports = router;
